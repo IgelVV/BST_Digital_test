a4f82d2431453aecb0cb624abdabeb0d2197b3ef
@vadim_veshkin <vadim.veshkin@mail.ru>
2023-09-26 14:18:26 +0400

    deserializer is created

--- robots/views.py
+++ robots/views.py
@@ -1,25 +1,21 @@
-from django.core import serializers
+from django.core.serializers.base import DeserializationError
 from django.core.exceptions import ValidationError
 from django.views import View
 from django.http import HttpRequest, HttpResponse
 
+from robots.deserializer import RobotDeserializer
+
 
 class AddRobotView(View):
     def post(self, request: HttpRequest) -> HttpResponse:
-        data = f'[{{"model": "robots.robot", ' \
-               f'"fields": {str(request.body, encoding="utf-8")}}}]'
         try:
-            for obj in serializers.deserialize("json", data):
-                robot = obj.object
-                robot.serial = robot.model + "-" + robot.version
-                try:
-                    robot.full_clean()
-                    robot.save()
-                except ValidationError as e:
-                    print(e.__repr__())
-                    return HttpResponse(e, status=403)
-                break
-        except serializers.base.DeserializationError as e:
+            deserializer = RobotDeserializer(request.body)
+            deserializer.validate()
+            deserializer.save()
+        except DeserializationError as e:
+            print(e.__repr__())
+            return HttpResponse(e, status=403)
+        except ValidationError as e:
             print(e.__repr__())
             return HttpResponse(e, status=403)
 
