0f7f84a85b17056097d3c4a4e10abb1123f9e2e8
@vadim_veshkin <vadim.veshkin@mail.ru>
2023-09-26 19:17:37 +0400

    docstrings are added.

--- robots/views.py
+++ robots/views.py
@@ -6,5 +6,6 @@ from robots.services import RobotService
 
 class WeeklyReportView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
+        """Download report for last 7 days about Robot production."""
         response = RobotService().get_weekly_report_as_response()
         return response
